9e01fcd01feede03ff3b9bbd1a4226139b3c9f2b
Nikolay Zhegalin <nzhegalin@gmail.com>
2019-03-24 22:08:35 +0300

    Fix source URL of chgk db site

--- src/main/java/info/chgk/db/service/impl/TasksImporterImpl.java
+++ src/main/java/info/chgk/db/service/impl/TasksImporterImpl.java
@@ -1,5 +1,6 @@
 package info.chgk.db.service.impl;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -8,21 +9,24 @@ import info.chgk.db.xml.Search;
 import info.chgk.db.xml.Tournament;
 
 /**
-
- *
  * @author Nikolay Zhegalin
  */
 @Service
 public class TasksImporterImpl implements TasksImporter {
+
+    @Value("${chgk.db.url}")
+    private String sourceUrl;
+
     @Override
     public Search importTasks(int complexity) {
         RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForObject("https://pda.baza-voprosov.ru/xml/random/answers/types1/complexity" + complexity, Search.class);
+        return restTemplate.getForObject(sourceUrl + "/xml/random/answers/types1/complexity" + complexity,
+                Search.class);
     }
 
     @Override
     public Tournament importTour(String id) {
         RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForObject("https://pda.baza-voprosov.ru/tour/" + id + "/xml", Tournament.class);
+        return restTemplate.getForObject(sourceUrl + "/tour/" + id + "/xml", Tournament.class);
     }
 }
